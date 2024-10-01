505b210c589ac3def28ec6aa0f14fb5ea11dcda3
abubakar <edwardian.edu02@gmail.com>
2024-10-01 19:16:23 +0500

    my commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import './App.css';
-import { Routes , Route } from 'react-router-dom';
 import { Home, About, Blog, Pricing } from "./pages/pages";
 import Navbar from './components/Navbar/Navbar';
-
+import { Routes , Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 function App() {
   return (
     <>
+    <BrowserRouter>
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/blog/:slug" element={<Blog/>} />
-        <Route path="/pricing" element={<Pricing/>} />
+        <Route path="/About" element={<About/>} />
+        <Route path="Blog" element={<Blog/>} />
+        <Route path="Pricing" element={<Pricing/>} />
         {/* <Route path="*" element={<ErrorPage />} /> */}
       </Routes>
+    </BrowserRouter>
     </>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,42 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaFacebook,FaTwitter, FaInstagram, FaYoutube, FaGithub } from "react-icons/fa";
+import { IoIosCall } from "react-icons/io";
+import { MdEmail } from "react-icons/md";
 
 const Navbar = () => {
-  return (
+return (
     <>
-        <header>
-            <div className='logo'>Devolynx</div>
-            <div className='manu'>
-                <ul>
-                    <li>About</li>
-                    <li>Pricing</li>
-                    <li>Blog</li>
-                    <li>Services</li>
+    {/* top-bar */}
+    <div className='flex justify-between p-3 items-center'>
+        <div className='flex gap-4 cursor-pointer'>
+            <span className='flex items-center gap-1'> +1 (718) 73493 <IoIosCall /> </span>
+            <span className='flex items-center'>info@example.com <MdEmail /></span>
+        </div>
+        <div className='flex gap-3'>
+                <a href='/'><FaFacebook/></a>
+                <a href='/'><FaTwitter/></a>
+                <a href='/'><FaInstagram/></a>
+                <a href='/'><FaYoutube/></a>
+                <a href='/'><FaGithub/></a> 
+        </div>
+    </div>
+        <header className='flex justify-between p-5 bg-slate-100 text-black items-center'>
+            <div className='cursor-pointer text-lg'> <Link to="/">Devolynx</Link></div>
+            <div className=''>
+                <ul className='flex gap-x-5'>    
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/About">About</Link></li>
+                <li><Link to="/Blog">Blog</Link></li>
+                <li><Link to="/Pricing">Pricing</Link></li>
                 </ul>
             </div>
-            <div className='nav-btn'>
-                <button>Get a Quote</button>
+            <div>
+                <button className='bg-sky-600 px-2 py-1 rounded'>Get a Quote</button>
             </div>
         </header>
     </>
-  )
+)
 }
 
 export default Navbar
